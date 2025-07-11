098105414ee521904beaa17736c919ff45e7208c
Chandulal123 <cc.5759001@gmail.com>
2025-07-11 21:31:40 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,27 @@
+#@assignment 2  test 1 : Check if a Number is Even or Odd
+
+a=int(input('enter the number : '))
+
+
+if a % 2 == 0 :
+    print('even number')
+else :
+    print( 'odd number ')
+
+
+# assignment 2 test 2 :Sum of Integers from 1 to 50 Using a Loop
+
+sum=0
+for i in range(1,51) :
+    sum += i
+    print('the sum of numbers from 1 to 50 : ',sum)
+
+
+
+count=1
+while count>=5 :
+    print(count)
+
+
+
+
